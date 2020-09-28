89e05ff3fdc5fb999544fdd4acff0ce7374cae83
halawimaher <halawimaher@gmail.com>
2020-09-28 11:19:22 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let name = window.prompt("Please enter your name");
+let surName = window.prompt("Please enter your family name");
+let city = window.prompt("Please enter your city");
+alert("Hello " + name + " " + surName + " "  + "From " + city);

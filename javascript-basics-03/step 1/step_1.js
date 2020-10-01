39b1e9cedb9dd32287393450fae715060b6456e8
halawimaher <halawimaher@gmail.com>
2020-10-01 12:32:15 +0300

    step_1

--- javascript-basics-03/step 1/step_1.js
+++ javascript-basics-03/step 1/step_1.js
@@ -3,3 +3,6 @@ let img = document.getElementById("image1");
 img.addEventListener("mouseover", () => {
     img.style.border = "3px solid red";
 });
+img.addEventListener("mouseout", () => {
+    img.style.border = "none";
+});
